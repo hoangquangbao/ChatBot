5a430a3f7707cbaabaa8ab7ff6d6b4fa6dff88cf
Bao Hoang <hoangquangbao.developer@gmail.com>
2023-07-04 08:36:21 +0700

    Init ChatBot project

---
+++ ChatBot/ChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotApp.swift
+//  ChatBot
+//
+//  Created by Quang Bao on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
